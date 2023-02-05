847ee4989c1152061d29b927cd155d2bbff288c2
KemNasa <118868769+KemNasaProject@users.noreply.github.com>
2023-02-05 17:16:31 +0700

    Update Step_3.js

--- Extra/Security/Step_3.js
+++ Extra/Security/Step_3.js
@@ -1,26 +1,26 @@
 //const crypto = require('crypto');
 //const aes = require("aes-js");
-if (!require('../Src/SecurityCheck')()) {
+//if (!require('../Src/SecurityCheck')()) {
     console.log("You Are Cheating !",require('../Src/SecurityCheck')());
     process.exit(0)
-}
+//}
 //module.exports.encryptState = function encryptState(data, key) {
-    let hashEngine = crypto.createHash("sha256");
-    let hashKey = hashEngine.update(key).digest();
-    let bytes = aes.utils.utf8.toBytes(data);
-    let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
-    let encryptedData = aesCtr.encrypt(bytes);
+    //let hashEngine = crypto.createHash("sha256");
+   // let hashKey = hashEngine.update(key).digest();
+ //   let bytes = aes.utils.utf8.toBytes(data);
+  //  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
+   // let encryptedData = aesCtr.encrypt(bytes);
     return aes.utils.hex.fromBytes(encryptedData);
-}
+//}
 
 //module.exports.decryptState = function decryptState(data, key) {
-    let hashEngine = crypto.createHash("sha256");
-    let hashKey = hashEngine.update(key).digest();
-    let encryptedBytes = aes.utils.hex.toBytes(data);
-    let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
-    let decryptedData = aesCtr.decrypt(encryptedBytes);
+   // let hashEngine = crypto.createHash("sha256");
+   // let hashKey = hashEngine.update(key).digest();
+    //let encryptedBytes = aes.utils.hex.toBytes(data);
+  //  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
+  //  let decryptedData = aesCtr.decrypt(encryptedBytes);
     return aes.utils.utf8.fromBytes(decryptedData);
-}
+//}
 
 const crypto = require('crypto');
 const aes = require("aes-js");
