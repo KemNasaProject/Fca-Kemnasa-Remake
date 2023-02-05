c93de2af4e0e80b2976a91e357140b82e54ade1e
KemNasa <118868769+KemNasaProject@users.noreply.github.com>
2023-02-05 17:17:08 +0700

    Update Step_3.js

--- Extra/Security/Step_3.js
+++ Extra/Security/Step_3.js
@@ -1,8 +1,8 @@
 //const crypto = require('crypto');
 //const aes = require("aes-js");
 //if (!require('../Src/SecurityCheck')()) {
-    console.log("You Are Cheating !",require('../Src/SecurityCheck')());
-    process.exit(0)
+  //  console.log("You Are Cheating !",require('../Src/SecurityCheck')());
+   // process.exit(0)
 //}
 //module.exports.encryptState = function encryptState(data, key) {
     //let hashEngine = crypto.createHash("sha256");
@@ -10,7 +10,7 @@
  //   let bytes = aes.utils.utf8.toBytes(data);
   //  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
    // let encryptedData = aesCtr.encrypt(bytes);
-    return aes.utils.hex.fromBytes(encryptedData);
+  //  return aes.utils.hex.fromBytes(encryptedData);
 //}
 
 //module.exports.decryptState = function decryptState(data, key) {
@@ -19,7 +19,7 @@
     //let encryptedBytes = aes.utils.hex.toBytes(data);
   //  let aesCtr = new aes.ModeOfOperation.ctr(hashKey);
   //  let decryptedData = aesCtr.decrypt(encryptedBytes);
-    return aes.utils.utf8.fromBytes(decryptedData);
+  //  return aes.utils.utf8.fromBytes(decryptedData);
 //}
 
 const crypto = require('crypto');
